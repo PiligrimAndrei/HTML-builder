26a240db5fb0e5b7ad3b46f0c6223e62e4bba373
Andrei Krasko <andrei@krasko.by>
2022-05-20 12:26:51 +0300

    feat: 06-build-page start OK

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
 const fsPromises = require('fs/promises');
-
+const { writer } = require('repl');
 
 const writableHtml = fs.createWriteStream(path.resolve(__dirname, 'project-dist', 'index.html'));
 const writableCss = fs.createWriteStream(path.resolve(__dirname, 'project-dist', 'style.css'));
@@ -11,7 +11,6 @@ const htmlBuilder = async function () {
    const mkdir = await fsPromises.mkdir(path.resolve(__dirname, 'project-dist'), { recursive: true });
    const files = await fsPromises.readdir(path.resolve(__dirname, 'styles'), { withFileTypes: true });
    const htmlFiles = await fsPromises.readdir(path.resolve(__dirname, 'components'), { withFileTypes: true });
-
    let arr = [];
    for (const file of files) {
       if (file.isFile() && path.extname(file.name) == '.css') {
@@ -21,36 +20,29 @@ const htmlBuilder = async function () {
          })
       }
    }
-   /* const template = fs.createReadStream(path.resolve(__dirname, 'template.html'), 'utf-8')
-   template.on('data', (chunk) => {
-      arr += chunk;
-      console.log(arr);
-   })
- */
+
    const templatePath = path.join(__dirname, 'template.html');
-   fileToArray(templatePath).then((arr) => chaingeTemplate(arr)).catch(console.error);
-
-
-   function chaingeTemplate(arr) {
-      //console.log(arr);
-      arr.forEach(element => {
-         element.includes('{{')
-         console.log(element);
-      });
-      let htmlTag = 'footer';
-      console.log(htmlTag);
-      components(htmlTag);
-   }
-
-
-   function components(tag) {
+   fileToArray(templatePath).then((arr) => {
+      let i = 1;
       for (const file of htmlFiles) {
-         if (file.isFile() && (file.name) == tag + '.html') {
-            console.log('Удачно')
-         }
+         const filename = (file.name).substring(0, (file.name).length - 5);
+         const indexfile = arr.findIndex((element) => (element.trim() == '{{' + filename + '}}'));
+         const filePath = path.join(__dirname, 'components', file.name);
+         fileToArray(filePath).then((filearr) => {
+            if (indexfile > 0) {
+               arr.splice(indexfile, 1, filearr.join('\n'));
+               return arr;
+            }
+         }).then((arr) => {
+            if (i == htmlFiles.length) {
+               arr.forEach((element) => {
+                  writableHtml.write(element + '\n');
+               })
+            } else i++;
+         })
       }
-   }
 
+   })
 
    // file to Array
    async function fileToArray(filepath) {
@@ -70,3 +62,25 @@ const htmlBuilder = async function () {
    }
 }
 htmlBuilder();
+
+
+
+/*/const templatePath = path.join(__dirname, 'template.html');
+   fileToArray(templatePath).then((arr) => chaingeTemplate(arr)).then((arr) => console.log('arrOUT', arr));
+
+
+   function chaingeTemplate(arr) {
+      for (const file of htmlFiles) {
+         //   console.log('arr: ', arr)
+         const filename = (file.name).substring(0, (file.name).length - 5);
+         const indexfile = arr.findIndex((element) => (element.trim() == '{{' + filename + '}}'));
+         const filePath = path.join(__dirname, 'components', file.name);
+         //console.log(filePath);
+         fileToArray(filePath).then((filearr) => {
+            arr.splice(indexfile, 1, filearr.join("\n"));
+            console.log('arrIN', arr);
+         })
+         //console.log('indexfile:', indexfile, 'filearr:', filearr);
+      }
+      return arr;
+   }*/
